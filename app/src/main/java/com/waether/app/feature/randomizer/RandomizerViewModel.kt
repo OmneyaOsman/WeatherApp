b1b0ad80cc3e95848ac8ce3447d61e20b725e1c6
Omni <engomneya.osman@gmail.com>
2019-03-18 21:49:25 +0200

    adding Ticker

--- app/src/main/java/com/waether/app/feature/randomizer/RandomizerViewModel.kt
+++ app/src/main/java/com/waether/app/feature/randomizer/RandomizerViewModel.kt
@@ -3,6 +3,7 @@ package com.waether.app.feature.randomizer
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.omni.usescases.Ticker
+import com.omni.usescases.randomNumberGenerator
 
 
 private const val DEFAULT_VALUE = 0
@@ -10,19 +11,19 @@ private const val INCREMENT_BY_VALUE = 0
 
 class RandomizerViewModel : ViewModel() {
 
-    val numberLiveData = MutableLiveData<Int>()
     private val ticker = initializeTicker()
+    val numberLiveData = MutableLiveData<Int>()
 
 
 
     init {
         numberLiveData.value = DEFAULT_VALUE
+        ticker.start()
     }
 
     private fun initializeTicker()=
          Ticker {
-            val oldValue = numberLiveData.value ?: DEFAULT_VALUE
-            numberLiveData.postValue(oldValue + INCREMENT_BY_VALUE)
+            numberLiveData.postValue(randomNumberGenerator().toInt())
         }
 
     override fun onCleared() {
--- usescases/src/main/java/com/omni/usescases/Engine.kt
+++ usescases/src/main/java/com/omni/usescases/Engine.kt
@@ -9,7 +9,7 @@ private const val FINISH_AFTER_MILLIS = 10*60000L
 private const val INTERVAL_IN_MILLIS = 1000L
 
 class Ticker(private val onTicking: (Long) -> Unit) :
-    CountDownTimer(10000, 1000) {
+    CountDownTimer(FINISH_AFTER_MILLIS, INTERVAL_IN_MILLIS) {
 
 
     override fun onTick(millisUntilFinish: Long) {
